cd1f8ca90cb04fa0502aaf240f8af629b17b8bb0
Parneet Singh Sarao <parneet1650.be20@chitkara.edu.in>
2022-05-04 12:28:24 +0530

    Updates

---
+++ precache-manifest.5488008baa059b9c9bf541d796d42ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "506ab4de704dcc383a4b",
+    "url": "/my-app/static/css/main.0b9f4bdf.chunk.css"
+  },
+  {
+    "revision": "506ab4de704dcc383a4b",
+    "url": "/my-app/static/js/main.506ab4de.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/my-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/my-app/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "ad6099f2e84041bd8e59",
+    "url": "/my-app/static/js/runtime~main.ad6099f2.js"
+  },
+  {
+    "revision": "1319ec7b65c5360bc95be8fdf872a76d",
+    "url": "/my-app/index.html"
+  }
+];
